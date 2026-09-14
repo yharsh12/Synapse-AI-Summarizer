9d5c2f58ebd8700ec292fa63d5172f92ddfe18bc
yharsh12 <harshyadav121204@gmail.com>
2026-09-14 21:06:57 +0530

    Update Note.js

--- backend/models/Note.js
+++ backend/models/Note.js
@@ -1,9 +1,26 @@
 const mongoose=require("mongoose");
 const noteSchema=new mongoose.Schema({
-  title:String,
-  content:String,
-  tags:String,
-  summary:String,
+  userId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+  },
+  title:{
+    type:String,
+    default:""
+  },
+  content:{
+    type:String,
+    default:""
+  },
+  tags:{
+    type:String,
+    default:""
+  },
+  summary:{
+    type:String,
+    default:""
+  }
 },{
   timestamps:true
 });

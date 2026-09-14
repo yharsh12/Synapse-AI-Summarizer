e5e0307287f2b11da9b171e025d0fe6e76ce8fab
yharsh12 <harshyadav121204@gmail.com>
2026-09-14 21:15:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,23 @@ const gem=require("./gemini");
 const express=require("express");
 const cors=require("cors");
 const connectDB=require("./config/db");
+const cookieParser=require("cookie-parser");
 const noteRoutes=require("./routes/noteRoutes");
+const authRoutes=require("./routes/auth");
 const app=express();
 connectDB();
-app.use(cors());
+app.use(cors({
+  origin:process.env.FRONTEND_URL,
+  credentials:true
+}));
 app.use(express.json());
+app.use(cookieParser());
+app.use((req,res,next)=>{
+  console.log(req.method,req.url);
+  next();
+});
 app.use("/api/notes",noteRoutes);
+app.use("/api/auth",authRoutes);
 app.get("/",(req,res)=>{
   console.log("ROOT ROUTE HIT");
   res.send("HELLO FROM EXPRESS");
@@ -23,7 +34,7 @@ app.get("/test-gemini",async(req,res)=>{
     res.status(500).json({error:"Gemini failed"});
   }
 });
-const PORT=process.env.PORT || 5000;
+const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>{
   console.log(`Server running on http://localhost:${PORT}`);
 });
